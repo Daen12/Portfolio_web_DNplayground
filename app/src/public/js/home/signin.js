480db6b602146d83f979f80e151fc9efde9a7ce3
Daen12 <sdy971213@gmail.com>
2022-12-28 15:12:21 +0900

    signin function created

--- app/src/public/js/home/signin.js
+++ app/src/public/js/home/signin.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const usersName = document.querySelector("#name");
+const age = document.querySelector("#age");
+const id = document.querySelector("#id");
+const password = document.querySelector("#pw");
+const signinBtn = document.querySelector(".button-area");
+console.log(id);
+
+function signin() {
+    const req = {
+        name: usersName.value,
+        age: age.value,
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
+
+signinBtn.addEventListener("click", signin);
